4e367a76f6f63eeb8f6bc1053b68f9dca171006a
JaRoD41 <laurent.trinco@gmail.com>
2023-11-27 11:20:34 +0100

    add 2 images

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeroSection = () => {
+	return (
+		<section>
+			<div className='grid grid-cols-1 lg:grid-cols-12'>
+				<div className='col-span-7 place-self-center'>
+					<h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>Hello, I&apos;m Laurent </h1>
+					<p className='text-[#ADB7BE] text-lg lg:text-xl'>I&apos;m a front-end developer Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus impedit, sed quisquam ipsam velit amet.</p>
+				</div>
+        <div className='col-span-5'></div>
+			</div>
+		</section>
+	)
+}
+
+export default HeroSection
