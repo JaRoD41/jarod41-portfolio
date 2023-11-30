d23f0cbabfb5bcc07906db8435fd457ef8f11152
JaRoD41 <laurent.trinco@gmail.com>
2023-11-30 16:21:16 +0100

    create NavLink dynamic component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,18 @@ const Navbar = () => {
 	return (
 		<nav>
 			<div className='flex flex-wrap items-center justify-between mx-auto p-8'>
-				<Link href='/' className='text-5xl text-white font-semibold'>LOGO</Link>
-        <div className="menu"></div>
+				<Link href='/' className='text-5xl text-white font-semibold'>
+					LOGO
+				</Link>
+				<div className='menu hidden md:block md:w-auto' id='navbar'>
+					<ul>
+						<li>
+							<Link href='/' className='block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white'>
+								Home
+							</Link>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</nav>
 	)
