e3f58b981725252066fbdff5d87b0bfbf24de2e7
JaRoD41 <laurent.trinco@gmail.com>
2023-11-30 15:56:29 +0100

    updated tailwind config file to avoid deprecated colors warnings

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,7 @@
+"use client"
 import React from 'react'
 import Image from 'next/image'
+import { TypeAnimation } from 'react-type-animation'
 
 const HeroSection = () => {
 	return (
@@ -7,8 +9,24 @@ const HeroSection = () => {
 			<div className='grid grid-cols-1 sm:grid-cols-12'>
 				<div className='col-span-7 place-self-center text-center sm:text-left'>
 					<h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
-						<span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Hello, I&apos;m{' '} </span>
-						Laurent
+						<span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
+							Hello, I&apos;m{' '}
+						</span>
+						<TypeAnimation
+							sequence={[
+								// Same substring at the start will only be typed out once, initially
+								'Laurent',
+								1000, // wait 1s before replacing "Mice" with "Hamsters"
+								'Full-Stack Developer',
+								1000,
+								'Mobile Developer',
+								1000,
+							]}
+							wrapper='span'
+							speed={50}
+							style={{ fontSize: '2em', display: 'inline-block' }}
+							repeat={Infinity}
+						/>
 					</h1>
 					<p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl '>
 						I&apos;m a front-end developer Lorem ipsum dolor sit amet
